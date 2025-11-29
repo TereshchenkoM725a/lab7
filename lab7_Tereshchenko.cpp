5aff5f687341c0a0f55ef01f0cb839a35b22861d
TereshchenkoM725a <Mykhailo.Tereshchenko@infiz.khpi.edu.ua>
2025-11-29 09:21:13 +0200

    Add files via upload

---
+++ lab7_Tereshchenko.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double R;
+    int count = 0;
+
+    cout << "Enter R(>0):";
+    cin >> R;
+
+    for (int x = -(int)R; x <= (int)R; x++) {
+        for (int y = -(int)R; y <= (int)R; y++) {
+            if (x * x + y * y <= R * R) {
+                count++;
+            }
+        }
+    }
+
+    cout << "Points inside circle:" << count << endl;
+
+    return 0;
+}
+
